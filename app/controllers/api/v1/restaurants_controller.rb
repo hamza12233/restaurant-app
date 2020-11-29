c4d372cbcbfd11d1ecf659ee8c7a2c994c7fbb85
Hamza Saleem <hafizhamzasaleem4@gmail.com>
2020-11-29 19:13:48 +0500

    api hit database

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,19 +1,39 @@
 class Api::V1::RestaurantsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   # respond_to :json
   # before_action :set_restaurants, only:[:create ,:show, :edit, :destroy]
 
 
   def create
-    @restaurant = Restaurant.create(restaurant_params)
-    if @restaurant.save
-      puts "success!"
-      render jason: @restaurant
+    @restaurant = Restaurant.new(restaurant_params)
+    
+    if @restaurant.save  
+      render json: @restaurant
     else
-      puts "Sorry something went wrong"
+      render json:{ :errors => @restaurant.errors.full_messages }
     end
-
   end
 
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update
+      render json: {
+        message: "This quote has been updated successfully."
+        }
+    else
+      render json:{ :errors => @restaurant.errors.full_messages }
+    end
+  end
+
+  def index
+    @restaurants = Restaurant.all
+    render json: @restaurants
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    render json: @restaurant
+  end
 
 private
   def restaurant_params
